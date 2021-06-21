c785481bab8dd5b07f38e072e613b3246a6c3ea3
jeffssant <santana.jeff@gmail.com>
2021-06-21 18:16:01 -0300

    desenho da pagina de zoom

--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from "mapbox-gl";
+
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+    .map-container{
+      width: 100%;
+      height: 100%;
+    }
+
+    .row{
+      background-color: white;
+      position: fixed;
+      bottom: 50px;      
+      left: 50px;
+      border-radius: 5px;
+      padding: 10px;
+      z-index = 99999;
+    }
+    `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +29,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+    container: 'mapZoom',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-46.451020460515885 , -23.533707897122152 ],
+    zoom: 16
+    });
   }
 
 }
