338e04c27eec2af4f85fb06f2b5201dd55f9ca39
jeffssant <santana.jeff@gmail.com>
2021-06-22 11:18:19 -0300

    adicionar marcador

--- src/app/maps/pages/marks/marks.component.ts
+++ src/app/maps/pages/marks/marks.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+
+import * as mapboxgl from "mapbox-gl";
 
 @Component({
   selector: 'app-marks',
   templateUrl: './marks.component.html',
   styles: [
+    `
+    .map-container{
+      width: 100%;
+      height: 100%;
+    }
+
+    .row{
+      background-color: white;
+      position: fixed;
+      bottom: 50px;      
+      left: 50px;
+      border-radius: 5px;
+      padding: 10px;
+      z-index = 99999;
+    }
+    `
   ]
 })
-export class MarksComponent implements OnInit {
+export class MarksComponent implements AfterViewInit {
+
+  @ViewChild('map') divMap!:ElementRef
+  maps!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number,number] = [-46.451020460515885 , -23.533707897122152 ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.maps = new mapboxgl.Map({
+    container: this.divMap.nativeElement,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: this.center,
+    zoom: this.zoomLevel
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat(this.center)
+      .addTo(this.maps)
   }
+
+ 
 
 }
